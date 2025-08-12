4d05609135b4a1230057b404de580828149a409a
lorainecastro <lrntrcstr@gmail.com>
2025-08-12 09:51:52 +0800

    commit

---
+++ generate.php
@@ -0,0 +1,21 @@
+<?php
+require 'vendor/autoload.php';
+
+use Endroid\QrCode\QrCode;
+use Endroid\QrCode\Writer\PngWriter;
+
+$qrCode = new QrCode('lorainecastro123');
+
+$writer = new PngWriter();
+$result = $writer->write($qrCode);
+
+$savePath = 'qrcodes/lorainecastro.png';  // relative path without __DIR__
+
+if (!file_exists('qrcodes')) {
+    mkdir('qrcodes', 0777, true);
+}
+
+$result->saveToFile($savePath);
+
+echo "QR Code saved to <strong>$savePath</strong><br>";
+echo "<img src='$savePath' alt='QR Code'>";

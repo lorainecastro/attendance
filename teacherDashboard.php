1f58e29fb63f6fbcf7e656949cd50406301dfb16
lorainecastro <lrntrcstr@gmail.com>
2025-08-25 11:17:13 +0800

    commit

--- teacherDashboard.php
+++ teacherDashboard.php
@@ -20,10 +20,8 @@ $profileName = htmlspecialchars($currentUser['firstname'] . ' ' . $currentUser['
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Student Attendance Monitoring System</title>
-
     <!-- Font Awesome CDN -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 
@@ -128,7 +126,7 @@ $profileName = htmlspecialchars($currentUser['firstname'] . ' ' . $currentUser['
 
         .system-title {
             font-size: var(--font-size-xl);
-            font-weight: 600;
+            font-weight: 700;
             color: #1a1a1a;
             display: flex;
             align-items: center;

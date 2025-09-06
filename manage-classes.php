c8014e4fb078099bcd70afa889db2fc43a30d1bb
lorainecastro <lrntrcstr@gmail.com>
2025-09-07 01:39:11 +0800

    commit

--- manage-classes.php
+++ manage-classes.php
@@ -2676,12 +2676,24 @@ ob_end_flush();
                         <label class="class-form-label" for="gradeLevel">Grade Level</label>
                         <select class="class-form-select" id="gradeLevel" required>
                             <option value="">Select Grade</option>
+                            <option value="Kindergarten">Kindergarten</option>
+                            <option value="Grade 1">Grade 1</option>
+                            <option value="Grade 2">Grade 2</option>
+                            <option value="Grade 3">Grade 3</option>
+                            <option value="Grade 4">Grade 4</option>
+                            <option value="Grade 5">Grade 5</option>
+                            <option value="Grade 6">Grade 6</option>
                             <option value="Grade 7">Grade 7</option>
                             <option value="Grade 8">Grade 8</option>
                             <option value="Grade 9">Grade 9</option>
                             <option value="Grade 10">Grade 10</option>
                             <option value="Grade 11">Grade 11</option>
                             <option value="Grade 12">Grade 12</option>
+                            <option value="College 1st Year">College 1st Year</option>
+                            <option value="College 2nd Year">College 2nd Year</option>
+                            <option value="College 3rd Year">College 3rd Year</option>
+                            <option value="College 4th Year">College 4th Year</option>
+                            <option value="College 5th Year">College 5th Year</option>
                         </select>
                     </div>
                     <div class="class-form-group">
